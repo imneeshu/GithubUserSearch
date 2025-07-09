d16209e02adb83215403f0770e244d6c86bc3f1b
Neeshu Kumar <neeshu@chingari.io>
2025-07-09 10:20:20 +0530

    Initial Commit

---
+++ GitHubUserSearch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitHubUserSearch
+//
+//  Created by Neeshu Kumar Chingari on 09/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitHubUserSearch/GitHubUserSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubUserSearchApp.swift
+//  GitHubUserSearch
+//
+//  Created by Neeshu Kumar Chingari on 09/07/25.
+//
+
+import SwiftUI
+
+@main
+struct GitHubUserSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
